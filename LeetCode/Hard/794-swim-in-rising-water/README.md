9d6b5dedd2db2f524adf407c19ff1bb85d5af2d1
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2026-07-29 00:56:01 +0530

    Swim in Rising Water: Hard (cpp)

---
+++ LeetCode/Hard/794-swim-in-rising-water/README.md
@@ -0,0 +1,19 @@
+# Swim in Rising Water
+
+![Difficulty](https://img.shields.io/badge/Difficulty-Hard-EF4444) ![Language](https://img.shields.io/badge/Language-cpp-2D3748)
+
+**Problem:** [leetcode.com/problems/swim-in-rising-water](https://leetcode.com/problems/swim-in-rising-water/)
+
+**Tags:** Array, Binary Search, Depth-First Search, Breadth-First Search, Union-Find, Heap (Priority Queue), Matrix
+
+## Submission
+- **Runtime:** 7 ms (beats 61.5%)
+- **Memory:** 12.8 MB (beats 75.6%)
+- **Submitted:** 2026-07-28 19:25:54 UTC
+
+## Approach
+_Add your notes on the approach here._
+
+## Complexity
+- **Time:** _TODO_
+- **Space:** _TODO_
---
+++ LeetCode/Hard/794-swim-in-rising-water/solution.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int x[4] = {-1,1,0,0};
+    int y[4] = {0,0,-1,1};
+
+    bool valid(int i,int j, int n , int m){
+        if(i<0 or i>=n or j<0 or j>=m){
+            return false;
+        }
+        return true;
+    }
+    int swimInWater(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+
+        vector<vector<int>> res(n);
+
+        for(int i = 0; i < n; i++){
+            vector<int> t(m, INT_MAX);
+            res[i] = t;
+        }
+
+        priority_queue<
+            pair<int, pair<int,int>>,
+            vector<pair<int, pair<int,int>>>,
+            greater<pair<int, pair<int,int>>>
+        > pq;
+
+        res[0][0] = a[0][0];
+        pq.push({a[0][0], {0,0}});
+
+        while(!pq.empty()){
+
+            pair<int, pair<int,int>> p = pq.top();
+            pq.pop();
+
+            int dist = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+
+            if(dist > res[row][col])
+                continue;
+
+            for(int k = 0; k < 4; k++){
+                int r = row + x[k];
+                int c = col + y[k];
+                if(!valid(r,c,n,m))
+                    continue;
+                int newTime = max(dist, a[r][c]);
+                if(newTime < res[r][c]){
+                    res[r][c] = newTime;
+                    pq.push({newTime, {r,c}});
+                }
+            }
+        }
+        return res[n-1][m-1];
+    }
+};
