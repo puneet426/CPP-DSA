9cc35ddc0102ff779bd143f35253230a563c96f3
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2026-08-06 15:48:37 +0530

    Insert Interval: Medium (cpp)

---
+++ LeetCode/Medium/57-insert-interval/README.md
@@ -0,0 +1,19 @@
+# Insert Interval
+
+![Difficulty](https://img.shields.io/badge/Difficulty-Medium-FACC15) ![Language](https://img.shields.io/badge/Language-cpp-2D3748)
+
+**Problem:** [leetcode.com/problems/insert-interval](https://leetcode.com/problems/insert-interval/)
+
+**Tags:** Array
+
+## Submission
+- **Runtime:** 3 ms (beats 30.7%)
+- **Memory:** 21.6 MB (beats 95.6%)
+- **Submitted:** 2026-08-06 10:18:29 UTC
+
+## Approach
+_Add your notes on the approach here._
+
+## Complexity
+- **Time:** _TODO_
+- **Space:** _TODO_
---
+++ LeetCode/Medium/57-insert-interval/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        for(int i=0;i<intervals.size();i++){
+            if(intervals[i][0]<=ans.back()[1])
+            ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            else
+            ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+};
