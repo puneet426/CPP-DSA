fe0e59893e0542199a4c653558b457028125271b
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2026-08-16 16:34:15 +0530

    Longest Common Subsequence: Medium (cpp)

---
+++ LeetCode/Medium/1250-longest-common-subsequence/README.md
@@ -0,0 +1,19 @@
+# Longest Common Subsequence
+
+![Difficulty](https://img.shields.io/badge/Difficulty-Medium-FACC15) ![Language](https://img.shields.io/badge/Language-cpp-2D3748)
+
+**Problem:** [leetcode.com/problems/longest-common-subsequence](https://leetcode.com/problems/longest-common-subsequence/)
+
+**Tags:** String, Dynamic Programming, Longest Common Subsequence
+
+## Submission
+- **Runtime:** 30 ms (beats 40.7%)
+- **Memory:** 13 MB (beats 85.4%)
+- **Submitted:** 2026-08-16 11:04:05 UTC
+
+## Approach
+_Add your notes on the approach here._
+
+## Complexity
+- **Time:** _TODO_
+- **Space:** _TODO_
---
+++ LeetCode/Medium/1250-longest-common-subsequence/solution.cpp
@@ -0,0 +1,22 @@
+// memoization
+
+class Solution {
+public:
+ int t[1001][1001];
+     int lcs(string &text1, string &text2,int n, int m){
+        if(n==0||m==0)
+        return 0;
+        if(t[n][m]!=-1)
+        return t[n][m];
+        if(text1[n-1]==text2[m-1])
+        return t[n][m] = 1+lcs(text1,text2,n-1,m-1);
+        else
+        return t[n][m] = max(lcs(text1,text2,n-1,m),lcs(text1,text2,n,m-1));
+     }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        memset(t,-1,sizeof(t));
+        return lcs(text1,text2,n,m);
+    }
+};
