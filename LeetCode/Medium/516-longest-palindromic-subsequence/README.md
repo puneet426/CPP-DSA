8a50e525a83cc386d42936f2832698cd1058adeb
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2026-08-17 18:58:00 +0530

    Longest Palindromic Subsequence: Medium (cpp)

---
+++ LeetCode/Medium/516-longest-palindromic-subsequence/README.md
@@ -0,0 +1,19 @@
+# Longest Palindromic Subsequence
+
+![Difficulty](https://img.shields.io/badge/Difficulty-Medium-FACC15) ![Language](https://img.shields.io/badge/Language-cpp-2D3748)
+
+**Problem:** [leetcode.com/problems/longest-palindromic-subsequence](https://leetcode.com/problems/longest-palindromic-subsequence/)
+
+**Tags:** String, Dynamic Programming
+
+## Submission
+- **Runtime:** 59 ms (beats 66.0%)
+- **Memory:** 13.2 MB (beats 84.7%)
+- **Submitted:** 2026-08-17 13:27:24 UTC
+
+## Approach
+_Add your notes on the approach here._
+
+## Complexity
+- **Time:** _TODO_
+- **Space:** _TODO_
---
+++ LeetCode/Medium/516-longest-palindromic-subsequence/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int t[1002][1002];
+    
+    int lcs(string &s, string &revs,int m,int n){    
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0||j==0)
+                t[i][j] = 0;
+            }
+        }
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s[i-1]==revs[j-1])
+                t[i][j] = 1 + t[i-1][j-1];
+                else{
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
+    }
+    int longestPalindromeSubseq(string s) {
+        int m = s.size();
+        string revs = s;
+        reverse(revs.begin(), revs.end());
+        int n = revs.size();
+        return lcs(s,revs,m,n);
+    }
+};
